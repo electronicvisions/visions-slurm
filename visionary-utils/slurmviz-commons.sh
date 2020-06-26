electronicvisions/slurm

Set deployment branch after successful install
* Add a new command to slurmviz-deployer: `update_deployed_branch`
  This updates the origin remote branch refs/heads/deployed/<install type> to
  point to the current HEAD. Install type refers to the root under which
  the given slurm instance is located (currently, the production system
  resides under /opt/slurm-skretch, hence the install type is
  slurm-skretch, whereas all testing systems are under
  /opt/slurm-<foobar>-testing).

* If issuing the `all` command, `update_deployed_branch` will be run
  after a scuccessful configure/compile/deploy-run.

* `update_deployed_branch` will only update the deployed branch if
  /refs/heads/deployed/<install type> exists.

* The review remote  has a manually created symlinked branch that points
  from refs/heads/production to refs/heads/deployed/slurm-skretch,
  indicating that this is the current commit used in production.
  This is synced to gitviz.

* Updating the branch will only work for people who have the permission
  to create new branches in the visions-slurm repository!

Change-Id: I22fd08c5afc1f5c81aea312a6c4423ff8c1b0152

visionary-utils/slurmviz-commons.sh
@@ -37,6 +37,8 @@ if [ -z "${DEPLOY_ROOT:-}" ]; then
   DEPLOY_ROOT="${CLUSTERIZE_PREFIX}/deployed"
 fi
 
+GIT_REMOTE_DEPLOYMENT="review"
+
 # if file does not exist, it will be generated
 # NOTE: This corresponds to the location INSIDE the container!
 MODULE_FILE_STORE="/run/slurm/current_modules.sh"
@@ -323,4 +325,14 @@ export_slurm_conf() {
     | awk '{ printf("export SLURM_CONF=\"%s/slurm.conf\"\n", $0) }')
   # NOTE: There should only be one export line because only one slurm-*
   # directory is mounted in container!
+}
+
+check_git_branch_modifiable() {
+  # check if we are able to modify branches on the review remote site
+  git push "${GIT_REMOTE_DEPLOYMENT}" -f HEAD:refs/heads/dummy/test_if_branches_are_writable
+  git push "${GIT_REMOTE_DEPLOYMENT}" :refs/heads/dummy/test_if_branches_are_writable
+}
+
+get_deployed_branchname() {
+    echo -n "refs/heads/deployed/${CLUSTERIZE_INSTALL_TYPE}"
 }

electronicvisions/visions-slurm

visionary-utils/slurmviz-commons.sh
@@ -37,6 +37,8 @@ if [ -z "${DEPLOY_ROOT:-}" ]; then
   DEPLOY_ROOT="${CLUSTERIZE_PREFIX}/deployed"
 fi
 
+GIT_REMOTE_DEPLOYMENT="review"
+
 # if file does not exist, it will be generated
 # NOTE: This corresponds to the location INSIDE the container!
 MODULE_FILE_STORE="/run/slurm/current_modules.sh"
@@ -323,4 +325,21 @@ export_slurm_conf() {
     | awk '{ printf("export SLURM_CONF=\"%s/slurm.conf\"\n", $0) }')
   # NOTE: There should only be one export line because only one slurm-*
   # directory is mounted in container!
+}
+
+check_git_branch_modifiable() {
+  local rc
+  # check if we are able to modify branches on the review remote site
+  git push "${GIT_REMOTE_DEPLOYMENT}" -f HEAD:refs/heads/deployed/dummy
+  rc=$?
+  if [ ${rc} -eq 0 ]; then
+    # deleting a branch in gerrit - while succeeding in the git repo - always
+    # returns an internal server error -> ignore
+    git push "${GIT_REMOTE_DEPLOYMENT}" :refs/heads/deployed/dummy
+  fi
+  return ${rc}
+}
+
+get_deployed_branchname() {
+    echo -n "refs/heads/deployed/${CLUSTERIZE_INSTALL_TYPE}"
 }